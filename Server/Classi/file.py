7c65f5e3c82645d629c97e63c3e8c384712b4d1f
Riccardo2110 <93716683+Riccardo2110@users.noreply.github.com>
2022-03-30 11:46:48 +0200

    Add files via upload

--- Server/Classi/file.py
+++ Server/Classi/file.py
@@ -1,1 +1,20 @@
+import hashlib
 
+class File:
+    def __init__(self, fileName, filePath):
+        self.fileName = fileName
+        self.filePath = filePath
+
+    def get_md5 (self):
+        file = open(self.fileName, 'rb')
+        dati = file.read()
+        md5 = ''
+        md5 = hashlib.md5(dati).hexdigest()
+        return md5
+
+    def check_md5(self, md5):
+        result = False
+        md5_file = self.get_md5()
+        if(md5 == md5_file):
+            result = True
+        return result
