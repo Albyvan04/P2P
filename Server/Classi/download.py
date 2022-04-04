719ab2e6a560626618c7b2453ed309c37c3703c2
STUDENTE216 <Email>
2022-04-04 11:18:24 +0200

    robette

--- Server/Classi/download.py
+++ Server/Classi/download.py
@@ -1,7 +1,16 @@
 class Download:
-    __init__(self, sessionId, md5file)
-        self.sessionId = sessionId
-        self.md5file = md5file
+    __init__(self, sessionId, md5file):
+        self.set_sessionId = sessionId
+        self.set_md5file = md5file
 
     get_sessionId(self):
         return self.sessionId
+
+    get_md5file(self):
+        return self.md5file
+
+    set_sessionId(self, sessionId):
+        self.sessionId = sessionId
+
+    set_md5file(self, md5file):
+        self.md5file = md5file
