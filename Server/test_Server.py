0696f4c4f1989beabf549e2a2aae6a13fb07ddb0
Riccardo2110 <93716683+Riccardo2110@users.noreply.github.com>
2022-04-14 08:35:56 +0200

    Update test_Server.py
    modifichine

--- Server/test_Server.py
+++ Server/test_Server.py
@@ -1,5 +1,6 @@
-from http import server
 import unittest
+from unittest import result
+from urllib import request
 from Classi.server import *
 from Classi.peer import *
 from Classi.download import *
@@ -29,6 +30,30 @@ class TestServer(unittest.TestCase):
     #    request = "DELF91e563b059504597a7105cb3b5ca3e831ee392edaa56916b064a8ba6b070eeebdba69d5b0e1e090d1e398336d022175a"
     #    nCopie, bol = Server.removeFile(request)
     #    self.assertEqual(bol, False, "Should be False")
+
+    def test_removeFile(self):
+        request = "DELFbc1037fcfb1613ce469e091bcae53752aa29ae32b6bfe4b7f7fe751bf8ee46a37ea54974c5ac5807d91ea16b1e4fc2c2"
+        bol = Server.removeFile(request)
+        self.assertEqual(bol, False, "Should be False") 
+
+    #def test_searchFile(self): DA RIVEDERE
+    #    request = "FIND55a6be7dabbb1dfe18556566492193fa7874fafda340660e8943b09dc7e34a4290417d06"
+    #    result = Server.searchFile(request)
+    #    self.assertEqual(result, 0, "Should be 0")
+
+    #def test_download(self): DA COMPLETARE
+    #    result = ""
+
+    def test_logout(self):
+        request = "LOGO00d740310ffa0758804a365822765076"
+        result = Server.logout(request)
+        self.assertEqual(result, False, "Should be False")
+
+    def test_regDownload(self):
+        request = "RREG00d740310ffa0758804a36582276507661799a05862487433fbe1643f774c87a78cdaeb5c669df7e4640bbeb5f8c90d627067c598655eebd62b8151b5a54637b81d25d22"
+        i, bol = Server.reg_download(request)
+        self.assertEqual(i, -1 , "Should be -1")
+        self.assertEqual(bol, False, "Should be False")
 
 
 if __name__ == "__main__":
